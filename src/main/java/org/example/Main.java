b60e72b718c8e2e1015863b78af853a37dbdc361
shahd293mohamed <moh861386@gmail.com>
2025-08-19 17:38:58 +0300

    Initial commit - Spring Boot Hello Controller

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+//package org.example;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
+
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
